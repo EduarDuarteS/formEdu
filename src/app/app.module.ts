eb7dd6889d435de82279708d9027cdd8a92f0298
Eduard Duarte <eduard.duarte@hotmail.com>
2019-02-25 16:51:02 -0500

    Se agregan modulos y links separados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { BindingComponent } from './binding/binding.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BindingComponent
+    BindingComponent,
+    FormularioComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'binding', component: BindingComponent },
+      { path: 'form', component: FormularioComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
