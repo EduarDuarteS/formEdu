eab38b6fe77b16715eb77e56c5a34a9fa747aea5
Eduard <eduard.duarte@hotmail.com>
2019-02-24 19:38:09 -0500

    se agrega modulo para binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { BindingComponent } from './binding/binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindingComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule
   ],
   providers: [],
---
+++ src/app/binding/binding.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-binding',
+  templateUrl: './binding.component.html',
+  styles: []
+})
+export class BindingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
