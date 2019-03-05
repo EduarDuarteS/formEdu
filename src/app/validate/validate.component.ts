9a86822f23f930262ee642f5b836b59609caf55d
Eduard Duarte <eduard.duarte@hotmail.com>
2019-03-05 10:27:06 -0500

    mostrar class en consola

--- src/app/validate/validate.component.ts
+++ src/app/validate/validate.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValidateComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  // console.log(pNombre.className);
+}
 
   ngOnInit() {
   }
+  logEd(objMostrar:any){
+    console.log(objMostrar);
+  }
+
 
 }
